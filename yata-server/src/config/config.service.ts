c4f6b59205b33c010372d49f6875289977e24f4e
Michael Bui <michaelbui99@hotmail.com>
2025-03-08 21:39:28 +0100

    yata-server: only load config once

--- yata-server/src/config/config.service.ts
+++ yata-server/src/config/config.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import * as os from 'os';
-import {DEFAULT_CONFIG_PATH, defaultConfig, YataConfig} from './yata-config';
+import { DEFAULT_CONFIG_PATH, defaultConfig, YataConfig } from './yata-config';
 import * as fs from 'fs';
 import * as path from 'path';
 
 @Injectable()
 export class ConfigService {
+  private config: YataConfig | undefined;
   private readonly YATA_CONFIG_ENV_VAR = 'YATA_CONFIG';
 
   public getConfig(): YataConfig {
+    if (!this.config) {
+      this.config = this.loadConfig();
+    }
+    return this.config;
+  }
+
+  private loadConfig(): YataConfig {
     const configPath =
       process.env[this.YATA_CONFIG_ENV_VAR] ?? DEFAULT_CONFIG_PATH;
     if (!fs.existsSync(path.resolve(configPath))) {
